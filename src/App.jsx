b7683ef73be1b823fd7c13e5496ca482823daa08
mdiksann <mdiksann@gmail.com>
2026-06-17 10:52:54 +0700

    feat: implement code editor interface with tab navigation and file explorer panels

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import IDE from "./components/ide/IDE";
 import StarryBackground from "./components/StarryBackground";
 import portfolioData from "./data/portfolioData.json";
 
 function App() {
   return (
-    <div className="min-h-screen relative">
+    <div className="h-screen w-screen overflow-hidden relative">
       <StarryBackground />
-      <Header />
-      <Hero data={portfolioData} />
-      <About data={portfolioData} />
-      <Contact data={portfolioData} />
+      <IDE data={portfolioData} />
     </div>
   );
 }
